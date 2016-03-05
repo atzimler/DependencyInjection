4b9aa1176f5bca98a2aa73adf427f7567302bed3
Attila Tamas Zimler <hijaszu@icloud.com>
2016-03-05 18:26:47 +1100

    Fixed namespace.

--- ATZ.DependencyInjection/DependencyResolver.cs
+++ ATZ.DependencyInjection/DependencyResolver.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DependencyInjection
+namespace ATZ.DependencyInjection
 {
     public static class DependencyResolver
     {
