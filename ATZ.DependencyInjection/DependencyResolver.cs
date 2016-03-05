3ec9ff6c96d7ef0ab215cb9f1acd1c8572e79e28
Attila Tamas Zimler <hijaszu@icloud.com>
2016-03-05 18:00:06 +1100

    Initial commit for separating the library from its original codebase.

---
+++ ATZ.DependencyInjection/DependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public static class DependencyResolver
+    {
+        #region Private Variables
+        private static IKernel _instance;
+        #endregion
+
+        #region Public Properties
+        public static IKernel Instance
+        {
+            get { return _instance; }
+        }
+        #endregion
+
+        #region Constructors
+        static DependencyResolver()
+        {
+            _instance = new StandardKernel();
+        }
+        #endregion
+    }
+}
