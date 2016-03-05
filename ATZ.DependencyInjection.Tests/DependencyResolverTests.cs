13d3c58bbebc17966e4763d6a1411ed9ad498b5e
Attila Tamas Zimler <hijaszu@icloud.com>
2016-03-05 18:14:43 +1100

    Added first unit test, so Test result analysis can be switched on on the build server.

---
+++ ATZ.DependencyInjection.Tests/DependencyResolverTests.cs
@@ -0,0 +1,20 @@
+﻿using DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATZ.DependencyInjection.Tests
+{
+    [TestFixture]
+    public class DependencyResolverTests
+    {
+        [Test]
+        public void KernelInstanceIsNotNull()
+        {
+            Assert.IsNotNull(DependencyResolver.Instance);
+        }
+    }
+}
