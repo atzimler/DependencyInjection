e070060407afe00e317ccb2ed4df3bde0aadcef4
Attila Tamas Zimler <hijaszu@icloud.com>
2016-10-13 07:17:40 +1100

    Code cleanup.

--- ATZ.DependencyInjection/DependencyResolver.cs
+++ ATZ.DependencyInjection/DependencyResolver.cs
@@ -1,30 +1,14 @@
 ﻿using Ninject;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ATZ.DependencyInjection
 {
     public static class DependencyResolver
     {
-        #region Private Variables
-        private static IKernel _instance;
-        #endregion
+        public static IKernel Instance { get; }
 
-        #region Public Properties
-        public static IKernel Instance
-        {
-            get { return _instance; }
-        }
-        #endregion
-
-        #region Constructors
         static DependencyResolver()
         {
-            _instance = new StandardKernel();
+            Instance = new StandardKernel();
         }
-        #endregion
     }
 }
