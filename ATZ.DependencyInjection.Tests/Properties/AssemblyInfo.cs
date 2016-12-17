64628337abbc36800d26aa0f67941b47aa1ef4d1
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2016-12-18 09:42:38 +1100

    Changing assembly versioning to support new build system for replacing * with the build number instead of time dependent generated number. This will allow us to have the same 4th number for all target framework.

--- ATZ.DependencyInjection.Tests/Properties/AssemblyInfo.cs
+++ ATZ.DependencyInjection.Tests/Properties/AssemblyInfo.cs
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("024c3405-73f5-4298-a108-4e45be568315")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.*")]
--- ATZ.DependencyInjection/Properties/AssemblyInfo.cs
+++ ATZ.DependencyInjection/Properties/AssemblyInfo.cs
@@ -20,23 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c9b6752a-9521-4f18-a315-70b4fe8e8bb6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-// Semantic versioning - from http://semver.org
-// Summary
-//Given a version number MAJOR.MINOR.PATCH, increment the:
-//MAJOR version when you make incompatible API changes,
-//MINOR version when you add functionality in a backwards-compatible manner, and
-//PATCH version when you make backwards-compatible fixes.
-//Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
-[assembly: AssemblyVersion("1.3.1.*")]
