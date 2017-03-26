11d49b860d7d457b3d7753fc03086255fbd7c110
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-03-26 19:00:54 +1100

    Setting up package building for ATZ.DependencyInjection.System.Windows.

--- ATZ.DependencyInjection.System.Windows/IMessageBox.cs
+++ ATZ.DependencyInjection.System.Windows/IMessageBox.cs
@@ -1,34 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ATZ.DependencyInjection.System.Windows
 {
+    /// <summary>
+    /// Interface for replacing System.Windows.MessageBox static object, so that its calls
+    /// can be tested for TDD.
+    /// </summary>
     public interface IMessageBox
     {
+        // ReSharper disable UnusedMember.Global => Part of public API.
+        /// <see cref="MessageBox.Show(string)"/>
         MessageBoxResult Show(string messageBoxText);
+        /// <see cref="MessageBox.Show(string, string)"/>
         MessageBoxResult Show(string messageBoxText, string caption);
+        /// <see cref="MessageBox.Show(string, string, MessageBoxButton)"/>
         MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button);
+        /// <see cref="MessageBox.Show(string, string, MessageBoxButton, MessageBoxImage)"/>
         MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon);
+        /// <see cref="MessageBox.Show(string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult)"/>
         MessageBoxResult Show(
             string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon,
             MessageBoxResult defaultResult);
+        /// <see cref="MessageBox.Show(string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult, MessageBoxOptions)"/>
         MessageBoxResult Show(
             string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon,
             MessageBoxResult defaultResult, MessageBoxOptions options);
 
+        /// <see cref="MessageBox.Show(Window, string)"/>
         MessageBoxResult Show(Window owner, string messageBoxText);
+        /// <see cref="MessageBox.Show(Window, string, string)"/>
         MessageBoxResult Show(Window owner, string messageBoxText, string caption);
+        /// <see cref="MessageBox.Show(Window, string, string, MessageBoxButton)"/>
         MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button);
+        /// <see cref="MessageBox.Show(Window, string, string, MessageBoxButton, MessageBoxImage)"/>
         MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon);
+        /// <see cref="MessageBox.Show(Window, string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult)"/>
         MessageBoxResult Show(
             Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon,
             MessageBoxResult defaultResult);
+        /// <see cref="MessageBox.Show(Window, string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult, MessageBoxOptions)"/>
         MessageBoxResult Show(
             Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon,
             MessageBoxResult defaultResult, MessageBoxOptions options);
+        // ReSharper restore UnusedMember.Global
     }
 }
--- ATZ.DependencyInjection.System.Windows/SystemWindowsMessageBox.cs
+++ ATZ.DependencyInjection.System.Windows/SystemWindowsMessageBox.cs
@@ -1,72 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ATZ.DependencyInjection.System.Windows
 {
+    /// <summary>
+    /// IDebug implementation that calls the System.Windows.MessageBox class.
+    /// </summary>
     public class SystemWindowsMessageBox : IMessageBox
     {
+        /// <see cref="MessageBox.Show(string)"/>
         public MessageBoxResult Show(string messageBoxText)
         {
             return MessageBox.Show(messageBoxText);
         }
 
+        /// <see cref="MessageBox.Show(string, string)"/>
         public MessageBoxResult Show(string messageBoxText, string caption)
         {
             return MessageBox.Show(messageBoxText, caption);
         }
 
+        /// <see cref="MessageBox.Show(string, string, MessageBoxButton)"/>
         public MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button)
         {
             return MessageBox.Show(messageBoxText, caption, button);
         }
 
+        /// <see cref="MessageBox.Show(string, string, MessageBoxButton, MessageBoxImage)"/>
         public MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon)
         {
             return MessageBox.Show(messageBoxText, caption, button, icon);
         }
 
+        /// <see cref="MessageBox.Show(string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult)"/>
         public MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon,
             MessageBoxResult defaultResult)
         {
             return MessageBox.Show(messageBoxText, caption, button, icon, defaultResult);
         }
 
+        /// <see cref="MessageBox.Show(string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult, MessageBoxOptions)"/>
         public MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon,
             MessageBoxResult defaultResult, MessageBoxOptions options)
         {
             return MessageBox.Show(messageBoxText, caption, button, icon, defaultResult, options);
         }
 
+        /// <see cref="MessageBox.Show(Window, string)"/>
         public MessageBoxResult Show(Window owner, string messageBoxText)
         {
             return MessageBox.Show(owner, messageBoxText);
         }
 
+        /// <see cref="MessageBox.Show(Window, string, string)"/>
         public MessageBoxResult Show(Window owner, string messageBoxText, string caption)
         {
             return MessageBox.Show(owner, messageBoxText, caption);
         }
 
+        /// <see cref="MessageBox.Show(Window, string, string, MessageBoxButton)"/>
         public MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button)
         {
             return MessageBox.Show(owner, messageBoxText, caption, button);
         }
 
+        /// <see cref="MessageBox.Show(Window, string, string, MessageBoxButton, MessageBoxImage)"/>
         public MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon)
         {
             return MessageBox.Show(owner, messageBoxText, caption, button, icon);
         }
 
+        /// <see cref="MessageBox.Show(Window, string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult)"/>
         public MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon,
             MessageBoxResult defaultResult)
         {
             return MessageBox.Show(owner, messageBoxText, caption, button, icon, defaultResult);
         }
 
+        /// <see cref="MessageBox.Show(Window, string, string, MessageBoxButton, MessageBoxImage, MessageBoxResult, MessageBoxOptions)"/>
         public MessageBoxResult Show(Window owner, string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon,
             MessageBoxResult defaultResult, MessageBoxOptions options)
         {
