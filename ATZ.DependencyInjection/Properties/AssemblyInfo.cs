39ce79e9d5d5c632d2e89dd2b1f91975bee0789e
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-07-01 16:04:18 +1000

    Correcting assembly information.

--- ATZ.DependencyInjection/Properties/AssemblyInfo.cs
+++ ATZ.DependencyInjection/Properties/AssemblyInfo.cs
@@ -7,22 +7,9 @@ using System.Resources;
 [assembly: AssemblyTitle("ATZ.DependencyInjection")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Attila Tamas Zimler")]
 [assembly: AssemblyProduct("ATZ.DependencyInjection")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
