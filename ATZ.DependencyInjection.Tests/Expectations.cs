b9c57c4fcfc819252930bab35948b7f3224102d1
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-07-01 13:49:24 +1000

    Adding Expectations.

---
+++ ATZ.DependencyInjection.Tests/Expectations.cs
@@ -0,0 +1,16 @@
+﻿using Ninject;
+using NUnit.Framework;
+
+namespace ATZ.DependencyInjection.Tests
+{
+    [TestFixture]
+    public class Expectations
+    {
+        [Test]
+        public void StandardKernelConstructorShouldNotCrash()
+        {
+            // ReSharper disable once ObjectCreationAsStatement => We are testing the constructor.
+            Assert.DoesNotThrow(() => new StandardKernel());
+        }
+    }
+}
